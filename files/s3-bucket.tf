d2f8e8f93865514dbda5cdb90bac1aa4b67817a7
Goh CP <gcheepeng@hotmail.com>
2022-09-30 07:37:43 +0000

    s3-bucket

--- files/s3-bucket.tf
+++ files/s3-bucket.tf
@@ -1,4 +1,8 @@
-module "s3-bucket" {
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
   source  = "app.terraform.io/dsta-cp/s3-bucket/aws"
-  version = "2.8.0"
+  version = "3.4.0"
+
+  bucket_prefix = "GohCP"
 }
